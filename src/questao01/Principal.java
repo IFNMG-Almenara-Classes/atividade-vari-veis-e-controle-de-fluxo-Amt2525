7ba703781f42f6790178da308e9c7119d936855f
Amt2525 <amt3@aluno.ifnmg.edu.br>
2025-08-01 22:10:08 -0300

    Resolução questão 1.

--- src/questao01/Principal.java
+++ src/questao01/Principal.java
@@ -1,7 +1,23 @@
 package questao01;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Digite o primeiro número:");
+        int n1 = entrada.nextInt();
+        System.out.print("Digite o segundo número:");
+        int n2 = entrada.nextInt();
+
+        if(n1>n2){
+            System.out.println("O maior número é:" + n1);
+        }else if(n2>n1){
+            System.out.println("O maior número é:" + n2);
+        }else{
+            System.out.println("Números iguais");
+        }
+
     }
 }
