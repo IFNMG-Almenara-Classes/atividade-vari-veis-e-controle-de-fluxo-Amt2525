7f32769f83972cbaf49e40c7e159631cbc835dfb
Amt2525 <amt3@aluno.ifnmg.edu.br>
2025-08-05 20:23:40 -0300

    Resolução questao 10

---
+++ src/questao10/Principal.java
@@ -0,0 +1,12 @@
+package questao10;
+import java.util.Scanner;
+public class Principal {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+       for (int i = 1; i<=50; i++){
+        System.out.print(i + " ");
+       }
+       sc.close();
+    }
+}
