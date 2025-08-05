9903e05caf563102e176ac654a1720cedf31999c
Amt2525 <amt3@aluno.ifnmg.edu.br>
2025-08-05 20:23:02 -0300

    Resolução questão 8

---
+++ src/questao08/Principal.java
@@ -0,0 +1,14 @@
+package questao08;
+
+import java.util.Scanner;
+
+public class Principal {
+   public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        for(int i = 1; i<=50;i++){
+        System.out.println(i);
+        }
+        entrada.close();
+    } 
+}
