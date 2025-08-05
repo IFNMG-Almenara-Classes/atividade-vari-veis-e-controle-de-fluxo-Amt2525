3ef9a0777287658322d803a38a2f09f84b248120
Amt2525 <amt3@aluno.ifnmg.edu.br>
2025-08-05 20:22:35 -0300

    Resoluçãi questão 6

---
+++ src/questão06/Principal.java
@@ -0,0 +1,29 @@
+package questão06;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Informe o primeiro número: ");
+        int n1 = entrada.nextInt();
+
+        System.out.println("Informe o segundo número: ");
+        int n2 = entrada.nextInt();
+
+        System.out.println("Informe o terceiro número: ");
+        int n3 = entrada.nextInt();
+
+        if(n1<n2 && n1<n3){
+            System.out.println("O menor é: "+n1);
+        }else if(n2<n1 && n2<n3){
+            System.out.println("O menor é: "+n2);
+        }else{
+            System.out.println("O menor é: "+n3);
+        }
+
+        entrada.close();
+    }
+}
+
